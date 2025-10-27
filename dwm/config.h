364b0f8b1d7eb1dcf433b1d0441187187e8ac214
clearlyyy <thorntonjaiden5@gmail.com>
2025-10-26 21:51:41 -0700

    working build

--- dwm/config.h
+++ dwm/config.h
@@ -5,8 +5,8 @@ static const unsigned int borderpx  = 2;        /* border pixel of windows */
 static const unsigned int snap      = 32;       /* snap pixel */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
-static const char *fonts[]          = { "Terminess Nerd Font Mono:weight=bold:size=28", "monospace:size=10" };
-static const char dmenufont[]       = "monospace:size=10";
+static const char *fonts[]          = { "Terminess Nerd Font Mono:weight=bold:size=12", "monospace:size=10" };
+static const char dmenufont[]       = "Terminess Nerd Font Mono:weight=bold:size=12"; 
 static const char col_gray1[]       = "#181818";
 static const char col_gray2[]       = "#222222";
 static const char col_gray3[]       = "#bbbbbb";
